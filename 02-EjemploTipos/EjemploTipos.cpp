14d3e5bc19b1e34803f919fe26e9d5930a488638
Andresbottero89 <38111038+Andresbottero89@users.noreply.github.com>
2018-04-23 12:45:14 -0300

    Update EjemploTipos.cpp

--- 02-EjemploTipos/EjemploTipos.cpp
+++ 02-EjemploTipos/EjemploTipos.cpp
@@ -4,9 +4,17 @@ Andrés Martín Bottero
 20180419
 */
 
-#include <iostream>
-#include <cassert>
+#include<iostream>
+#include<cassert>
+#include<math.h>                //Esta libreria la invoco para poder utilizar operaciones matemáticas básicas.
+
 using namespace std;
+
+double redondear(double);       //Prototipo de una función
+
+double redondear(double num){   //Se define la función
+return round(num);              //Redondeara la variable que luego reemplazará a "num"
+}
 
 int main(){
 
@@ -17,13 +25,9 @@ const int room=237;
 const double precio=299.99;
 const string mensaje="Conocete a ti mismo";
 
-assert(amor==true);
-assert(sigla=='n');
-assert(maximo==65535);
-assert(room==237);
-assert(precio==299.99);
-assert(mensaje=="Conocete a ti mismo");
-
-//cout<<amor<<"\n"<<sigla<<"\n"<<maximo<<"\n"<<room<<"\n"<<precio<<"\n"<<mensaje;
-
+assert(amor!=false and sigla=='n');     //Amor es verdadero. Si se ingresa una palabra en el tipo de dato char entonces solo se guardara la ultima letra.
+assert((maximo%room)>mensaje.length()); //Compara el resto de una división con la longitud de una frase.
+assert(precio!=redondear(precio));      //El valor de precio no debe ser igual al valor de precio redondeado.
+assert(-(room-precio)==precio-room);    //Comparo negativos.
+assert(room*room*room==pow(room,3));
 }
