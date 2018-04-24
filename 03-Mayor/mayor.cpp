45b23e6b226ff956ca33cb34e46bdeda42327134
Andresbottero89 <38111038+Andresbottero89@users.noreply.github.com>
2018-04-24 10:27:38 -0300

    Create mayor.cpp

---
+++ 03-Mayor/mayor.cpp
@@ -0,0 +1,18 @@
+/*Mayor de dos números
+Trabajo Práctico 3
+Andrés Martín Bottero
+20180424
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+int num1,num2;
+cout<<"Ingrese un numero: ";
+cin>>num1;
+cout<<"Ingrese otro numero: ";
+cin>>num2;
+cout<<num1<<"+"<<num2<<"="<<num1+num2;
+}
